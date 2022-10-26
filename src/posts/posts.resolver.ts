06cd030d39d3db8d99739c714bf932e523b657d3
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2022-10-26 11:52:23 +0200

    create post mutation

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,5 +1,5 @@
-import { Post } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Post } from './post.entity';
 import { PostsService } from './posts.service';
 
 @Resolver(() => Post)
@@ -9,5 +9,10 @@ export class PostsResolver {
   @Query(() => String)
   post() {
     return 'post';
+  }
+
+  @Mutation(() => Post)
+  createPost(@Args('title') title: string, @Args('authorId') authorId: string) {
+    return this.postsService.createPost(title, authorId);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postsRepository: Repository<Post>,
+  ) {}
+
+  createPost(title: string, authorId: string) {
+    const post = this.postsRepository.create({
+      title,
+      author: {
+        id: authorId,
+      },
+    });
+
+    return this.postsRepository.save(post);
+  }
+}
